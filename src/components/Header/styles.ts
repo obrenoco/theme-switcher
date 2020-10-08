776d336ec3b37f5a5b8375262b468c425409f78a
Your Name <bsromeiro@gmail.com>
2020-10-08 11:18:13 -0300

    feat | add : header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    height: 60px;
+    background-color: #F24423;
+    display: flex;
+    align-items: center;
+    padding: 0 30px;
+`;
